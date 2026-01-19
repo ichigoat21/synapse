a4d1c9c32a235e6588eecf7f8a822d1e25512d8b
shivansh <shivresides@gmail.com>
2026-01-20 02:53:15 +0530

    deleted ep

--- backend/src/routes/content.ts
+++ backend/src/routes/content.ts
@@ -4,6 +4,7 @@ import contentModel from "../schema/content";
 const contentRouter = Router()
 
 contentRouter.post("/add", async (req, res)=> {
+    try {
     const link = req.body.link
     const title = req.body.title
     const type = req.body.type
@@ -17,17 +18,44 @@ contentRouter.post("/add", async (req, res)=> {
     res.status(200).json({
         message : 'Content Added',
         content
-    })
+    }) } catch (err) {
+        console.error("SIGNUP ERROR:", err);
+        res.status(500).json({
+          message: "Server Error",
+        });
+      }
 })
 
 contentRouter.get("/home", async (req, res)=> {
+    try {
     const userId = req.id
     const content = await contentModel.find({
         userId : userId
     })
     res.json({
         content
+    }) } catch (err) {
+        console.error("SIGNUP ERROR:", err);
+        res.status(500).json({
+          message: "Server Error",
+        });
+      }
+})
+
+contentRouter.delete("/content/:id", async (req, res)=> {
+    try {
+    const id = req.params.id
+    await contentModel.deleteOne({
+        _id : id
     })
+    res.status(200).json({
+        message : 'Item Deleted'
+    }) } catch (err) {
+        console.error("SIGNUP ERROR:", err);
+        res.status(500).json({
+          message: "Server Error",
+        });
+      }
 })
 
 
