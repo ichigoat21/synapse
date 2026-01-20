5eb4b37e24b2c0f6e99136e82208ebd0890d39f3
shivansh <shivresides@gmail.com>
2026-01-20 16:44:02 +0530

    link schema

---
+++ backend/src/schema/link.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+
+const linkSchema = new Schema({
+    hash : {type : String, required : true},
+    userId : {type : Types.ObjectId, ref : "Users", required : true }
+})
+
+const linkModel = mongoose.model("Link", linkSchema)
+
+export default linkModel
