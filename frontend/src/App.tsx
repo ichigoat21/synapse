bfbd8f3a28c21c16d107d50a96495946ce4bd102
shivansh <shivresides@gmail.com>
2026-01-22 04:11:22 +0530

    ui development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,15 @@ function App() {
     setModalOpen(!modalOpen)
   }
   return <div className="inset-0 h-screen bg-bgwhite">
-    <Button onclick={()=>{modalHandler()}} variant="primary" size="md" text="Add Content"/>
+    <div className="w-screen flex items-center justify-between ">
+      <div className="p-12">
+        <p className="font-sans text-2xl font-Arimo font-bold">All Notes</p>
+      </div>
+      <div className="flex items-center gap-6 p-12">
+      <Button onclick={()=>{modalHandler()}} variant="primary" size="md" text="Add Content"/>
+      <Button onclick={()=>{modalHandler()}} variant="secondary" size="md" text="Share Brain"/>
+      </div>
+    </div>
     <Modal onClose={modalHandler} open={modalOpen}/>
   </div>
 }
