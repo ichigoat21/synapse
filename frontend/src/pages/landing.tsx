84cb1e7327272bdfe137a5168bffeca4bff2a101
shivansh <shivresides@gmail.com>
2026-02-04 08:24:48 +0530

    added substance

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,33 +1,41 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from "../components/Button";
-import { LogoImg } from "../logo/logo";
+import { useNavigate } from "react-router-dom"
+import { Button } from "../components/Button"
+import { LogoImg } from "../logo/logo"
 
-
-export function LandingPage(){
+export function LandingPage() {
     const navigate = useNavigate()
-    return <div>
-    <header>
-        <div className="flex items-center justify-between mt-10 mx-5 md:mx-20">
+  
+    return (
+      <div className="min-h-screen flex flex-col">
+        <header>
+          <div className="flex items-center justify-between mt-10 mx-5 md:mx-20">
             <p className="font-display text-2xl md:text-3xl">Synapse</p>
-            <Button variant="primary" size="md" text="Get Started" onclick={()=> {
-                navigate("/signup")
-            }}/>
-        </div>
-    </header>
-    <main>
-        <div className="flex justify-between items-center mx-60 mt-20">
-            <p className="font-display text-5xl">
-                Never worry about forgetting 
-                <br />
-                Your highlights from the Internet
-            </p>
+            <Button
+              variant="primary"
+              size="md"
+              text="Get Started"
+              onclick={() => navigate("/signup")}
+            />
+          </div>
+        </header>
+  
+        <main className="flex-1">
+          <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mx-10 md:mx-60 mt-20">
             <div>
-                <LogoImg/>
+              <LogoImg />
             </div>
-        </div>
-    </main>
-    <footer>
-
-    </footer>
-    </div>
-}
+            <p className="font-display text-3xl md:text-5xl text-center sm:text-left">
+              Never worry about forgetting
+              <br />
+              Your highlights from the Internet
+            </p>
+          </div>
+        </main>
+  
+        <footer className="w-full bg-black py-6 flex items-center justify-center">
+          <p className="text-white text-sm">Developed by shiv</p>
+        </footer>
+      </div>
+    )
+  }
+  
